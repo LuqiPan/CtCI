e7470cabb5c3bf4b64a0a6820ebc48ce5033a183
Luqi Pan <luqi.pan@gmail.com>
2014-01-19 15:43:58 -0500

    Merge 1.1 and 1.3 to one file. - Gonna use one java file for each question. - May be a little bit easier to test.

---
+++ C01-Arrays-and-Strings/ChapterOne.java
@@ -0,0 +1,87 @@
+import java.util.Arrays;
+import java.io.*;
+
+public class ChapterOne {
+    //1.1 string has all unique characters
+    public static boolean hasUniqueChar(String str){
+        //determine the length upfront
+        if (str.length() > 128) return false;
+
+        //record the appearance of each char
+        boolean[] present = new boolean[128];
+
+        for (int i = 0; i < str.length(); i++){
+            int val = str.charAt(i);
+            if (present[val]) return false;
+            present[val] = true;
+        }
+
+        return true;
+    }
+
+    //-------------------------------------------------------
+
+    public static boolean hasUniqueChar2(String str){
+        //bit manipulation
+        int checker = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            int val = str.charAt(i) - 'a';
+
+            //pay attention to $, << and |
+            if ((checker & (1 << val)) > 0) {
+                return false;
+            }
+
+            checker |= (1 << val);
+        }
+        
+        return true;
+    }
+
+    //=======================================================
+
+    //1.3 decide if one is a permutation of the other
+
+    public static String sort(String original) {
+        //sort the string
+        //utilize Arrays.sort method
+        char[] chars = original.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+
+    public static boolean isPerm(String a, String b) {
+        //decide if it's permutation
+        //compare the length upfront
+        if (a.length() != b.length()) return false;
+
+        return sort(a).equals(sort(b));
+    }
+
+    //-------------------------------------------------------
+
+    public static boolean isPerm2(String a, String b) {
+        //count appearance of each char
+        if (a.length() != b.length()) return false;
+
+        int[] counts = new int[128];
+
+        for (int i = 0; i < a.length(); i++)
+            counts[a.charAt(i)]++;
+
+        for (int i = 0; i < b.length(); i++) {
+            int val = b.charAt(i);
+            if ( --counts[val] < 0 ) return false;
+        }
+
+        return true;
+    }
+
+    //=======================================================
+
+    public static void main(String[] args) {
+        System.out.println(ChapterOne.isPerm2("abc", "bac"));
+    }
+
+}
